db3c03c9f57760ef9c8ac21c0b90818e628dfb56
bilal_himammy <62145376ww@gmail.com>
2025-08-15 14:46:15 +0300

    Fix paths for GitHub Pages

--- payment-lp-main/next.config.mjs
+++ payment-lp-main/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   basePath: '/umrah-landing',
   assetPrefix: '/umrah-landing/',
+  images: {
+    unoptimized: true, // ضروري للـ export
+  },
 };
 
 export default nextConfig;
